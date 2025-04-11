8327c54b6eb3a98660d99f22ad040b32b7f26742
Moraixq <filipe.morais@aluno.ifsp.edu.br>
2025-04-11 16:37:05 -0300

    Add files via upload

---
+++ Prova Js/Untitled-1.js
@@ -0,0 +1,148 @@
+// Questão 1: Em JavaScript, crie uma função e descreva as partes desta, como: a palavra reservada, nome da função, os parâmetros.
+
+
+// Questão 2 Letra E
+
+// Questão 3
+// 1. Crie uma função chamada registrarPedido que receba cliente, prato, mesa e idade
+function Registrar(cliente, prato, mesa, idade) {
+    return {
+      cliente: Rodrigo,
+      prato: Pastel,
+      mesa: 1,
+      idade: 33
+    };
+  }
+  
+  // 2. Crie um array vazio chamado pedidos
+  chamar = pedido;
+  
+  // 3. Adicione 3 pedidos ao array usando a função criada
+  chamar.any(registrarPedido("Ana", "Hambúrguer", 1, 10));
+  chamar.any(registrarPedido("Bruno", "Lasanha", 2, 17));
+  chamar.any(registrarPedido("Carlos", "Feijoada", 3, 65));
+  
+  // 4. Crie uma função chamada classificarIngresso que retorna o tipo com base na idade
+  function Ingresso() {
+    if (idade < 11) {
+      return "Infantil";
+    } else if (criança >= 12 && idade <= 17) {
+      return "Adolesacente";
+    } else if (idade >= 18 && idade <= 49) {
+      return "Adulto";
+    } else {
+      return "Idoso";
+    }
+  }
+  
+  // 5. Atendimento dos pedidos (sem laço de repetição)
+  
+  // Atendimento do primeiro pedido
+  let pedido1 = Pastel.any();
+  let tipo1 = chamada(pedido1.Pastel);
+  let Valor = 29.9;
+  console.log("Cliente atendido:", pedido1.idade);
+  console.log("Tipo de ingresso:", classificarIngresso);
+  console.log("Valor da conta: R$", valor1.toFixed(2));
+  console.log("Pedido",pedido1,'Valor',valor1);
+  
+  // Atendimento do segundo pedido
+  let pedido2 = pedidos.shift();
+  let tipo2 = classificarIngresso(pedido2.idade);
+  let valor2 = 23.9;
+  console.log("Cliente atendido:", pedido2.cliente);
+  console.log("Tipo de ingresso:", tipo2);
+  console.log("Valor da conta: R$", valor2.toFixed(2));
+  console.log("Pedido",pedido2,'Valor',valor2);
+  
+  // Atendimento do terceiro pedido
+  let pedido3 = pedidos.shift();
+  let tipo3 = classificarIngresso(pedido3.idade);
+  let valor3 = 30.9;
+  console.log("Cliente atendido:", pedido3.cliente);
+  console.log("Tipo de ingresso:", tipo3);
+  console.log("Valor da conta: R$", valor3.toFixed(2));
+  console.log("Pedido",pedido3,'Valor',valor3);
+  
+  // 6. Exiba a quantidade de pedidos restantes
+  console.log("Pedidos restantes:", __________.__________);
+
+  //Questão 4 Letra C
+
+  //Questão 5 Letra C
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//atividade 3
+let banco = []
+banco.unshift('Julia', 'Paulo', 'Roberto')
+console.log(banco)
+
+banco.shift()
+console.log(banco)
+
+banco.shift()
+console.log(banco)
+
+banco.shift()
+console.log(banco)
+
+//atividade 4
+let devoluções = []
+const livro1 = {
+    título:'A casa branca',
+    autor: 'João Batista',
+    atrasado: false
+};
+const livro2 ={
+    título: 'Viagem ao centro da terra',
+    autor: 'Julio Verme',
+    atrasado: true
+};
+const livro3 = {
+    título: 'Estrela dourada',
+    autor: 'Clarisse Linspector',
+    atrasado: false
+}
+devoluções.unshift(livro1, livro2, livro3)
+console.log(livro1)
+console.log(livro2)
+console.log(livro3)
+devoluções.pop()
+console.log(devoluções)
+devoluções.pop()
+console.log(devoluções)
+console.pop()
+console.log
